c21ab7f75397da060e5faa1a06bfa3f1ac384be2
Max Nilsson <Maxstjernbergnilsson@gmail.com>
2025-06-03 10:50:31 +0200

    Update of test.sql

--- test.sql
+++ test.sql
@@ -1,15 +1,13 @@
 BEGIN;
 
--- Sätt in en ny kund
+
 INSERT INTO customers (name, address, phone, ssn)
 VALUES ('Maxmaxmax', 'Testvägen 2', '+46123456789', '2323-1234');
 
--- Sätt in tillhörande konto
 INSERT INTO accounts (account_number, customer_id)
 VALUES ('SE8902001234567890433456', (SELECT id FROM customers WHERE ssn = '1234-2222'));
 
--- Om vi kommit hit, commit
 COMMIT;
 
 ROLLBACK;
--- Annars fångas felet nedan
+
