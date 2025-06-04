3a41aaee3fd19b0b2f848e7ba96269b0b32a2882
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-06-04 10:52:12 +0200

    added output colors

--- workflow.py
+++ workflow.py
@@ -14,16 +14,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 
-
 @task
 def create_tables():
     logger = get_run_logger()
     try:
-        logger.info(" Skapar tabeller...")
+        logger.info("\033[94m️  Skapar tabeller...\033[0m")
         Base.metadata.create_all(engine)
-        logger.info(" Tabeller skapade")
+        logger.info("\033[92m Tabeller skapade\033[0m")
     except SQLAlchemyError as e:
-        logger.error(f" Fel vid skapande av tabeller: {e}")
+        logger.error(f"\033[91m Fel vid skapande av tabeller: {e}\033[0m")
         raise
 
 
@@ -31,11 +30,11 @@ def create_tables():
 def load_customers(csv_path: str):
     logger = get_run_logger()
     try:
-        logger.info(f" Läser in kunder från {csv_path}")
+        logger.info(f"\033[94m️  Läser in kunder från {csv_path}\033[0m")
         load_customers_accounts(csv_path)
-        logger.info(" Kunder & konton inlästa")
+        logger.info("\033[92m Kunder & konton inlästa\033[0m")
     except Exception as e:
-        logger.error(f" Fel vid kundinläsning: {e}")
+        logger.error(f"\033[91m Fel vid kundinläsning: {e}\033[0m")
         raise
 
 
@@ -43,27 +42,27 @@ def load_customers(csv_path: str):
 def load_transactions_task(csv_path: str):
     logger = get_run_logger()
     try:
-        logger.info(f" Läser in transaktioner från {csv_path}")
+        logger.info(f"\033[94m️  Läser in transaktioner från {csv_path}\033[0m")
         load_transactions(csv_path)
-        logger.info(" Transaktioner inlästa")
+        logger.info("\033[92m Transaktioner inlästa\033[0m")
     except Exception as e:
-        logger.error(f" Fel vid transaktionsinläsning: {e}")
+        logger.error(f"\033[91m Fel vid transaktionsinläsning: {e}\033[0m")
         raise
 
 
 @flow(name="Populate Bank Database")
 def populate_normalized_database(
-        customer_csv: str = "data/sebank_customers_with_accounts.csv",
-        transaction_csv: str = "data/transactions.csv"
+    customer_csv: str = "data/sebank_customers_with_accounts.csv",
+    transaction_csv: str = "data/transactions.csv"
 ):
     logger = get_run_logger()
-    logger.info(" Startar Prefect-flow för datainläsning")
+    logger.info("\033[94m Startar Prefect-flow för datainläsning\033[0m")
 
     create_tables()
     load_customers(customer_csv)
     load_transactions_task(transaction_csv)
 
-    logger.info("Prefect-flow färdigt")
+    logger.info("\033[92m Prefect-flow färdigt\033[0m")
 
 
 if __name__ == "__main__":
