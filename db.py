8377e9b61c37a1842ddbc139b3c5805730eb5978
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-06-04 10:30:55 +0200

    my conn.string

--- db.py
+++ db.py
@@ -13,9 +13,9 @@ class Db:
     @staticmethod
     def _create_conn():
         return psycopg2.connect(
-            dbname='dbname',
-            user='user',
-            password='password',
+            dbname='bank',
+            user='postgres',
+            password='intedin',
             host='localhost',
             port='5432'
         )
